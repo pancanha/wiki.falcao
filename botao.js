b004d3475849707142baf0474ed9755bb408c0b4
dioniziojonathan <123213031+pancanha@users.noreply.github.com>
2024-08-21 15:25:40 -0300

    caminho andado

--- botao.js
+++ botao.js
@@ -1,9 +1,16 @@
-$('form').on('submit',function(e){
-    e.preventDefault();
-    var but = $(this).find('[type="submit"]').toggleClass('sending').blur();
-    
-    setTimeout(function(){
-       but.removeClass('sending').blur();
-    },4500);
-    
-  })
+$(function() {  
+   $('.btn-6')
+     .on('mouseenter', function(e) {
+          var parentOffset = $(this).offset(),
+             relX = e.pageX - parentOffset.left,
+             relY = e.pageY - parentOffset.top;
+          $(this).find('span').css({top:relY, left:relX})
+     })
+     .on('mouseout', function(e) {
+          var parentOffset = $(this).offset(),
+             relX = e.pageX - parentOffset.left,
+             relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+     });
+ });
+ 
